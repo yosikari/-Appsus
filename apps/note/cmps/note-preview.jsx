f30826f742a1159d562a64c85c65479f67eaa9c9
OritLeshem <azmono@gmail.com>
2022-12-28 09:17:34 -0500

    sub 16.00

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -1,24 +1,50 @@
 const { useRef, useState, useEffect } = React
 const { Outlet, Link, NavLink } = ReactRouterDOM
 export function NotePreview({ note }) {
-  const [cmpType, setCmpType] = useState('Hello')
+  console.log("hello from note preview")
+  const [cmpType, setCmpType] = useState(note.type)
   console.log(note)
+  useEffect(() => { }, [
+
+  ])
   return <article className="note-preview">
-    <h2>note id: {note.id}</h2>
+    {/* <h2>note id: {note.id}</h2>
     <h2>note type: {note.type}</h2>
 
     <h3> {note.info.title}</h3>
-    <img src={note.info.url} />
-    {/* <DynamicCmp cmpType={cmpType} /> */}
+    <img src={note.info.url} /> */}
+    <DynamicCmp note={note} cmpType={cmpType} />
   </article>
 }
-// function DynamicCmp(props) {//props is the note.type
-//   switch (props.cmpType) {
-//     case 'note-txt':
-//       return <NoteTxt {...props} />
-//     case 'note-img':
-//       return <NoteImg {...props} />
-//     case 'note-todo':
-//       return <NoteTodo {...props} />
-//   }
-// }
+function DynamicCmp(props) {//props is the note.type
+  console.log('DynamicCmp', props.cmpType)
+  switch (props.cmpType) {
+    case 'note-txt':
+      return <NoteTxt {...props} />
+    case 'note-img':
+      return <NoteImg {...props} />
+    case 'note-todo':
+      return <NoteTodo {...props} />
+  }
+}
+function NoteTxt(props) {
+  return <div>
+    <h4> {props.note.info.txt}</h4>
+    pinned: <h6> {props.note.isPinned}</h6>
+  </div>
+}
+function NoteImg(props) {
+  return <div>
+    <h4> {props.note.info.title}</h4>
+    <img src={props.note.info.url} />
+  </div>
+
+
+}
+function NoteTodo(props) {
+  return <div>
+    <h4> {props.note.info.label}</h4>
+    <p> {props.note.info.todos[0].txt}</p>
+
+  </div>
+}
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,4 +1,4 @@
-console.log('Hi from note service')
+// console.log('Hi from note service')
 import { storageService } from "../../../services/async-storage.service.js"
 import { utilService } from "../../../services/util.service.js"
 const NOTE_KEY = 'noteDB'
@@ -23,14 +23,14 @@ function _createNotes() {
   let notes = utilService.loadFromStorage(NOTE_KEY)
   if (!notes || !notes.length) {
     notes = [
-      //   {
-      //   id: "n101",
-      //   type: "note-txt",
-      //   isPinned: true,
-      //   info: {
-      //     txt: "Fullstack me Baby!"
-      //   }
-      // }, 
+      {
+        id: "n101",
+        type: "note-txt",
+        isPinned: true,
+        info: {
+          txt: "Fullstack me Baby!"
+        }
+      },
       {
         id: "n102",
         type: "note-img",
@@ -53,19 +53,151 @@ function _createNotes() {
           backgroundColor: "#00d"
         }
       },
-      //  {
-      //   id: "n103",
-      //   type: "note-todo",
+      {
+        id: "n103",
+        type: "note-todo",
 
-      //   info: {
-      //     label: "go to a vacation",
-      //     todos: [
-      //       { txt: "Driving liscence", doneAt: null },
-      //       { txt: "Coding power", doneAt: 187111111 }
-      //     ]
-      //   },
+        info: {
+          label: "go to a vacation:",
+          todos: [
+            { txt: "Driving liscence", doneAt: null },
+            { txt: "Coding power", doneAt: 187111111 }
+          ]
+        },
 
-      // }
+      },
+      {
+        id: "n105",
+        type: "note-txt",
+        isPinned: true,
+        info: {
+          txt: "Fullstack me Baby!"
+        }
+      },
+      {
+        id: "n106",
+        type: "note-img",
+        info: {
+          url: "../../../img/cruise.jpg",
+          title: "our cruise",
+        },
+        style: {
+          backgroundColor: "#00d"
+        }
+      },
+      {
+        id: "n107",
+        type: "note-img",
+        info: {
+          url: "../../../img/maldives.jpg",
+          title: "our vacation",
+        },
+        style: {
+          backgroundColor: "#00d"
+        }
+      },
+      {
+        id: "n108",
+        type: "note-todo",
+
+        info: {
+          label: "go to a vacation:",
+          todos: [
+            { txt: "Driving liscence", doneAt: null },
+            { txt: "Coding power", doneAt: 187111111 }
+          ]
+        },
+
+      },
+      {
+        id: "n109",
+        type: "note-txt",
+        isPinned: true,
+        info: {
+          txt: "Fullstack me Baby!"
+        }
+      },
+      {
+        id: "n110",
+        type: "note-img",
+        info: {
+          url: "../../../img/cruise.jpg",
+          title: "our cruise",
+        },
+        style: {
+          backgroundColor: "#00d"
+        }
+      },
+      {
+        id: "n111",
+        type: "note-img",
+        info: {
+          url: "../../../img/maldives.jpg",
+          title: "our vacation",
+        },
+        style: {
+          backgroundColor: "#00d"
+        }
+      },
+      {
+        id: "n112",
+        type: "note-todo",
+
+        info: {
+          label: "go to a vacation:",
+          todos: [
+            { txt: "Driving liscence", doneAt: null },
+            { txt: "Coding power", doneAt: 187111111 }
+          ]
+        },
+
+      },
+      {
+        id: "n113",
+        type: "note-txt",
+        isPinned: true,
+        info: {
+          txt: "Fullstack me Baby!"
+        }
+      },
+      {
+        id: "n114",
+        type: "note-img",
+        info: {
+          url: "../../../img/cruise.jpg",
+          title: "our cruise",
+        },
+        style: {
+          backgroundColor: "#00d"
+        }
+      },
+      {
+        id: "n104",
+        type: "note-img",
+        info: {
+          url: "../../../img/maldives.jpg",
+          title: "our vacation",
+        },
+        style: {
+          backgroundColor: "#00d"
+        }
+      },
+      {
+        id: "n115",
+        type: "note-todo",
+
+        info: {
+          label: "go to a vacation:",
+          todos: [
+            { txt: "Driving liscence", doneAt: null },
+            { txt: "Coding power", doneAt: 187111111 }
+          ]
+        },
+
+      }
+
+
+
 
 
     ]
