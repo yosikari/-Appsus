07942bf04277d41679bbadadaf7107ddf13cff82
yosikari <yosikari@gmail.com>
2022-12-31 22:06:44 +0200

    edit home page

--- views/home.jsx
+++ views/home.jsx
@@ -1,10 +1,21 @@
+const { Link, NavLink } = ReactRouterDOM
 
 export function Home() {
 
     return <section className="home">
 
-        <img className="homepage-notes-btn" src="img/notes-btn.png" alt="" />
-        <img className="homepage-mails-btn" src="img/mails-btn.png" alt="" />
+        <Link to="/note"> <img className="homepage-imgs" src="img/notes-btn.png" alt="" />        </Link>
+        <Link to="/mail">    <img className="homepage-imgs" src="img/mails-btn.png" alt="" />        </Link>
+        <a href="https://oritleshem.github.io/Meme-Generator/"><img className="homepage-imgs" src="img/meme-gen-btn.png" alt="" /></a>
+
+
+
 
     </section>
 }
+
+
+
+
+
+
